8b1e08eb3bcd687a66c8e8f464272c65723b61c6
Alexandre Angelim <angelim@angelim.com.br>
2011-07-13 01:26:56 -0300

    bumping version to 0.3.0

--- lib/mongoid-included/document_inclusion.rb
+++ lib/mongoid-included/document_inclusion.rb
@@ -22,7 +22,8 @@ module Mongoid
         raise DocumentAlreadyIncluded,  "Document already included" if (!included_by.blank? && included_by!=[_model_klass(_class_name)]) 
         embedded_in _model, args
         
-        (self.included_by ||= []) << _model_klass(_class_name) unless included_by.include? _model_klass(_class_name)
+        included_in ||= []
+        self.included_by << _model_klass(_class_name) unless included_by.include? _model_klass(_class_name)
         self.extend ActiveModel::Naming
         _overwrite_model_name
       end
